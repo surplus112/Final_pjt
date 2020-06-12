df630b4ece7316205ddf1bd0ee3dda68cadef6b7
kimjanghu <kjh921030@gmail.com>
2020-06-12 14:18:50 +0900

    Create article serializer

--- django-back/articles/models.py
+++ django-back/articles/models.py
@@ -13,5 +13,5 @@ class Article(models.Model):
   
 class Comment(models.Model):
     content = models.TextField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
