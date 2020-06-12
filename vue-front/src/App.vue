417c851ce0d37543a85541be222467c049b0c370
surplus112 <qhduddlsl@naver.com>
2020-06-12 16:52:54 +0900

    중간 업로드

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,9 +1,62 @@
 <template>
-  <div>
+  <div id="app">
     <div>
       <router-link to="/">Home</router-link> |
-      <h1>하이</h1>
+      <router-link to="/accounts/login">login</router-link>
     </div>
-    <router-view/>
+    <router-view
+      @submit-login-data="login"
+    />
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+const SERVER_URL = 'http://localhost:8000'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      isLoggedIn: false,
+    }
+  },
+  methods: {
+    setCookie(token) {
+      this.$cookies.set('auth-token', token)
+      this.isLoggedIn = true
+    },
+    signup(signupData) {
+      axios.post(SERVER_URL + '/rest-auth/signup', signupData)
+        .then(res => {
+          this.setCookie(res.datd.key)
+          this.$router.push({ name: 'Home' })
+        })
+        .catch(err => console.log(err.response.data))
+    },
+    login(loginData) {
+      axios.post(SERVER_URL + '/rest-auth/login/', loginData)
+        .then(res => {
+          this.setCookie(res.data.key)
+          this.$router.push({ name: 'Home'})
+        })
+        .catch(err => console.log(err.response.data))
+    },
+    logout() {
+      const requestHeaders = {
+        headers: {
+          Authorization: `Token ${this.$cookies.get('auth-token')}`
+        }
+      }
+      axios.post(SERVER_URL + '/rest-auth/logout/', null, requestHeaders)
+        .then(() => {
+          this.$cookies.remove('auth-token')
+          this.isLoggedIn = false
+          this.$router.push({ name: 'Home' })
+        })
+        .catch(err => console.log(err.response.data))
+    },
+  }
+}
+</script>
--- vue-front/src/router/index.js
+++ vue-front/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import LoginView from '../views/accounts/LoginView.vue'
+import LogoutView from '../views/accounts/LogoutView.vue'
+import SignupView from '../views/accounts/SignupView.vue'
+import UserUpdateView from '../views/accounts/UserUpdateView.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,6 +14,26 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/accounts/login',
+    name: 'LoginView',
+    component: LoginView
+  },
+  {
+    path: '/accounts/logout',
+    name: 'LogoutView',
+    component: LogoutView
+  },
+  {
+    path: '/accounts/signup',
+    name: 'SignupView',
+    component: SignupView
+  },
+  {
+    path: '/accounts/userupdate',
+    name: 'UserUpdateView',
+    component: UserUpdateView
   },
 ]
 
