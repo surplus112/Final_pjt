ae78ffc797291231b28073ddd45fcd4d9307257d
kimjanghu <kjh921030@gmail.com>
2020-06-12 14:03:03 +0900

    Create article and comment model

--- django-back/articles/models.py
+++ django-back/articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.comf import settings
+from movies.models import Movie
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+  
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
