776a4537b787c1e32a26b2f9efbae9e4480e18b8
kimjanghu <kjh921030@gmail.com>
2020-06-12 15:56:22 +0900

    Regist user model in admin page

--- django-back/movies/views.py
+++ django-back/movies/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import MovieSerializer, MovieListSerializer, GenreSerializer
 from .models import Movie, Genre
+from .permissions import IsSuperUser
 
 # Create your views here.
 @api_view(['GET'])
@@ -17,3 +18,11 @@ def movie_detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsSuperUser])
+def movie_create(request):
+    serializer = MovieSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data)
