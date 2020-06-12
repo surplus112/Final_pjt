dfb9f025e80825890c8ae6cc2d32e0a9727786b2
kimjanghu <kjh921030@gmail.com>
2020-06-12 14:34:42 +0900

    Create movie serializer

---
+++ django-back/movies/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Movie, Genre
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
