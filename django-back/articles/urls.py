ac132f6712f5d79077747cb69be9b2a703e10c42
kimjanghu <kjh921030@gmail.com>
2020-06-13 16:41:21 +0900

    Feature article update

--- django-back/articles/urls.py
+++ django-back/articles/urls.py
@@ -5,6 +5,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list),
-    path('create/', views.create_article),
+    path('create/', views.article_create),
     path('<int:article_pk>', views.article_detail),
 ]
--- django-back/articles/views.py
+++ django-back/articles/views.py
@@ -12,18 +12,29 @@ def article_list(request):
     serializer = ArticleListSerializer(articles, many=True)
     return Response(serializer.data)
 
-
 @api_view(['GET'])
 def article_detail(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     serializer = ArticleSerializer(article)
     return Response(serializer.data)
 
-
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def create_article(request):
+def article_create(request):
     serializer = ArticleSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
-        serializer.save(user=request.user)  # NOT NULL CONSTRAINT FAILED
+        serializer.save(user=request.user)
         return Response(serializer.data)
+
+@api_view(['PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def article_update(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    if request.method == 'PUT':
+        serializer = ArticleSerializer(data=request.data, instance=article)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            movie.delete()
+            return Response({'message':'Article has been deleted!'})
